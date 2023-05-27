59602c614a76729a61d09b008488d279105ba668
Yuzunoha <studysteadystandby@gmail.com>
2023-05-27 22:50:10 +0900

    こんなところか

--- public/main.js
+++ public/main.js
@@ -1,12 +1,15 @@
-let isInsertMode = false;
+btnEdit.onclick = () => {
+  modeSetEdit();
+};
+btnSend.onclick = () => {
+  modeSetDisp();
+};
 
-divDisp.onmouseup = () => {
-  if (isInsertMode) {
-    isInsertMode = false;
-    return;
-  }
-  if ('none' === divArea.style.display) {
-    divArea.style.display = 'block';
-    divDisp.style.display = 'none';
-  }
+const modeSetEdit = () => {
+  divArea.style.display = 'block';
+  divDisp.style.display = 'none';
+};
+const modeSetDisp = () => {
+  divArea.style.display = 'none';
+  divDisp.style.display = 'block';
 };
