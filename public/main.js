46496f9348cf134fdc43b519d230eee4adee4210
Yuzunoha <studysteadystandby@gmail.com>
2023-05-27 23:16:40 +0900

    ctlr+Enter

--- public/main.js
+++ public/main.js
@@ -8,16 +8,24 @@ btnSend.onclick = () => {
 const setStyleDisplayBulk = (elements, display) => {
   elements.forEach((element) => (element.style.display = display));
 };
-
 const setStyleDisplayBulkBlock = (elements) => setStyleDisplayBulk(elements, 'block');
 const setStyleDisplayBulkNone = (elements) => setStyleDisplayBulk(elements, 'none');
-
 const modeSetEdit = () => {
   setStyleDisplayBulkBlock([divArea, btnSend]);
   setStyleDisplayBulkNone([divDisp, btnEdit]);
 };
-
 const modeSetDisp = () => {
   setStyleDisplayBulkBlock([divDisp, btnEdit]);
   setStyleDisplayBulkNone([divArea, btnSend]);
 };
+
+document.addEventListener('keydown', (event) => {
+  if ('block' === btnSend.style.display) {
+    /* 編集モード中に */
+    if (event.ctrlKey && event.code === 'Enter') {
+      /* ctlr+Enter が押された*/
+      // 送信ボタン押下と同じ効果を発生させる
+      btnSend.onclick();
+    }
+  }
+});
