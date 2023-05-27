2153b1b964ca46786f72b475ebf3c7529c39d4f6
Yuzunoha <studysteadystandby@gmail.com>
2023-05-28 02:12:48 +0900

    繋がった

--- lambda_nodejs18/index.mjs
+++ lambda_nodejs18/index.mjs
@@ -1,7 +1,9 @@
 // Create service client module using ES6 syntax.
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-const ddbClient = new DynamoDBClient({});
-export { ddbClient };
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+const options = { endpoint: 'http://localhost:8000', region: 'dummy' };
+const dynamoDB = new DynamoDB(options);
+const documentClient = DynamoDBDocumentClient.from(dynamoDB);
 
 const p = console.log;
-p(123);
+p({ documentClient });
