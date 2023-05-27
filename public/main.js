948d1f533ddc7e3b0e371a23e2bad4a3b5193dd7
Yuzunoha <studysteadystandby@gmail.com>
2023-05-27 22:32:09 +0900

    リファクタ

--- public/main.js
+++ public/main.js
@@ -1,25 +1,12 @@
+let isInsertMode = false;
+
 disp.onmouseup = () => {
-  if (flg == '1') {
-    flg = '0';
-  } else {
-    if (area.style.display == 'none') {
-      area.style.display = 'block';
-      disp.style.display = 'none';
-    }
+  if (isInsertMode) {
+    isInsertMode = false;
+    return;
+  }
+  if ('none' === area.style.display) {
+    area.style.display = 'block';
+    disp.style.display = 'none';
   }
 };
-
-let flg = '0';
-function rep() {
-  let txt = document.getElementById('disp').innerHTML;
-  txt = txt.replace(
-    /(http:\/\/[\x21-\x7e]+)/gi,
-    '<a href="http://rd.nan7.net/$1" onmousedown="flg=' + "'1'" + ';" target="_blank">$1</a>'
-  );
-  txt = txt.replace(
-    /(https:\/\/[\x21-\x7e]+)/gi,
-    '<a href="http://rd.nan7.net/$1" onmousedown="flg=' + "'1'" + ';" target="_blank">$1</a>'
-  );
-  document.getElementById('disp').innerHTML = txt;
-}
-rep();
